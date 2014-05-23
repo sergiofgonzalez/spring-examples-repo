90e459833cd8efa15548b397ec5afa8afcaf7901
Sergio F. Gonzalez <sergio.f.gonzalez@gmail.com>
2014-05-23 11:27:10 +0200

    Added hello-spring-boot-web-security.

---
+++ hello-spring-boot-samples-parent/hello-spring-boot-web-security/src/main/java/org/joolzminer/examples/boot/Application.java
@@ -0,0 +1,93 @@
+package org.joolzminer.examples.boot;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@EnableAutoConfiguration
+@ComponentScan
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class Application extends WebMvcConfigurerAdapter {
+	
+	@Controller
+	protected static class HomeController {
+		@RequestMapping("/")
+		@Secured("ROLE_ADMIN")
+		public String home(Map<String, Object> model) {
+			model.put("message", "Hello, world!");
+			model.put("title", "Hello home");
+			model.put("date", new Date());
+			return "home";
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/access").setViewName("access");
+	}
+
+	@Configuration
+	protected static class AuthenticationSecurity extends GlobalAuthenticationConfigurerAdapter {
+
+		@Override
+		public void init(AuthenticationManagerBuilder auth) throws Exception {
+			auth.inMemoryAuthentication()
+				.withUser("admin")
+				.password("admin")
+				.roles("ADMIN", "USER")
+				.and()
+				.withUser("user")
+				.password("user")
+				.roles("USER");
+		}		
+	}
+	
+	protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests()
+					.antMatchers("/login")
+					.permitAll().anyRequest()
+					.fullyAuthenticated()
+					.and()
+					.formLogin()
+					.loginPage("/login")
+					.failureUrl("/login?error")
+					.and()
+					.logout()
+					.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+					.and()
+					.exceptionHandling()
+					.accessDeniedPage("/access?error");
+		}		
+	}
+	
+	@Bean
+	public ApplicationSecurity applicationSecurity() {
+		return new ApplicationSecurity();
+	}
+}
